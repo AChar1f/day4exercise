27352b60b81e5d8d33df0ccea43730887f258256
AChar1f <acharif41@gmail.com>
2024-05-17 09:21:44 +0200

    day 4 exercise data

---
+++ script/code.js
@@ -0,0 +1,6 @@
+let person1 = {firstName: "Richard", lastName: "Reeds"}
+console.log(person1)
+let person2 = {firstName: "Aron", lastName: "James"}
+console.log(person2)
+let register = [person1, person2]
+console.log(register)
